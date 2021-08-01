7b6f793d28e4fc57972385cce07c6c1f4c0d0afe
Nicolas Corti <nicolasecorti@gmail.com>
2021-07-31 21:41:34 -0300

    segundo commit

--- js/main.js
+++ js/main.js
@@ -1,27 +1,50 @@
+const iva= 1.21;
 const carrito = [];
+let total= 0;
 
-Object Producto (nombre, precio, cantidad){
-this.nombre:nombre;
-this.precio: precio;
-this.cantidad: cantidad;
+// Constructor de Producto
+function Producto (nombre, precio, cantidad){
+    this.nombre = nombre,
+    this.precio = precio,
+    this.cantidad = cantidad
 }
+// Inicio
+prompt("Bienvenido al sistema de compras. Se les  mostraran los productos por pantalla, ingrese la cantidad que desea de cada uno, al finalizar le indicará el total. Presione aceptar para continuar.");
 
+let cantidad1 = prompt('Remera tema cucurucho, precio lista $500. Cantidad:');
+let producto1 = new Producto('Remera tema cucurucho', 500, cantidad1);
+carrito.push(producto1);
 
-Let producto1= new Producto('Remera tema cucurucho', 500, 1);
+let cantidad2 = prompt('Remera tema sunday, precio lista $500. Cantidad:');
+let producto2 = new Producto('Remera tema sunday', 500, cantidad2);
+carrito.push(producto2);
 
-carrito[0]=producto1;
+let cantidad3 = prompt('Remera tema palito, precio lista $500. Cantidad:');
+let producto3 = new Producto('Remera tema palito', 500, cantidad3);
+carrito.push(producto3);
 
-Let producto2= new Producto('Calendario de helado', 70, 2);
+let cantidad4 = prompt('Remera tema helado, precio lista $500. Cantidad:');
+let producto4 = new Producto('Remera tema helado', 500, cantidad4);
+carrito.push(producto4);
 
-carrito[1]=producto2;
+let cantidad5 = prompt('Calendario de helado, precio lista $70. Cantidad:');
+let producto5 = new Producto('Calendario de helado', 70, cantidad5);
+carrito.push(producto5);
 
-Let producto3= new Producto('Poster de helado', 500, 1);
-carrito[2]=producto3;
+let cantidad6 = prompt('Poster de helado, precio lista $200. Cantidad:');
+let producto6 = new Producto('Poster de helado', 200, cantidad6);
+carrito.push(producto6);
 
-//Falta agregar el array carrito al foreach
+// Calculo total de carrito
+carrito.forEach(producto => {
+    total+= (producto.precio * producto.cantidad);
+});
+// agrego el Iva
+total= total*iva;
 
-for(producto of Producto){
-console.log("PRODUCTO: ",this.nombre);
-console.log("PRECIO: ",this.precio);
-console.log("CANTIDAD: "this.cantidad);
-}
+// Devuelvo el total
+prompt("El TOTAL de la compra es: $", total);
+
+carrito.forEach(producto => {
+    alert(producto.nombre, "precio: "+ producto.precio +"cantidad: " + producto.cantidad);
+});
